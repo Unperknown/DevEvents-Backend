b74b1d7cd3166337d25694360df04a834dd92c59
Unperknown <wnsdud105@gmail.com>
2020-02-01 22:07:57 +0900

    [3] Refactored : Code

--- db/index.js
+++ db/index.js
@@ -1,19 +1,20 @@
 const mongoClient = require('mongodb').MongoClient
 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'
+
 const config = { 
     useUnifiedTopology: true,
-    useNewUrlParser: true 
+    useNewUrlParser: true
 }
 
 module.exports = {
     client: null,
     database: null,
-    init: async () => {
+    init: async (databaseName, collectionName) => {
         this.client = await mongoClient
-            .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/', config)
+            .connect(MONGODB_URI, config)
             .catch(err => console.log(err))
-    },
-    set: (databaseName, collectionName) => {
+
         this.database = this.client.db(databaseName).collection(collectionName)
     },
     close: () => {
